3d037b72c36b8d9d2eae506939d833f50dcf53c9
ldevernay <boblehamster@yahoo.fr>
2019-10-10 10:54:14 +0200

    Seed thématiques par défaut

--- database/seeds/ThemeSeeder.js
+++ database/seeds/ThemeSeeder.js
@@ -12,10 +12,30 @@
 
 /** @type {import('@adonisjs/lucid/src/Factory')} */
 const Factory = use('Factory')
+const Theme = use('App/Models/Theme')
 
 class ThemeSeeder {
   async run () {
-    await Factory.model('App/Models/Theme').createMany(5);
+    let transport = new Theme();
+    transport.title = "Transport";
+    await transport.save();
+
+    let alim = new Theme();
+    alim.title = "Alimentation";
+    await alim.save();
+
+    let eau = new Theme();
+    eau.title = "Eau/Energie";
+    await eau.save();
+
+    let elec = new Theme();
+    elec.title = "Appareils électroniques";
+    await elec.save();
+
+    let achat = new Theme();
+    achat.title = "Nouveaux achats";
+    await achat.save();
+    // await Factory.model('App/Models/Theme').createMany(5);
   }
 }
 
